92b5f57d66482c72322611257996964564dc8eb4
Davyjones <ekenedavidson15@gmail.com>
2021-08-22 13:21:33 +0100

    enable disenable service

--- src/main/java/com/sabi/agent/service/repositories/StateRepository.java
+++ src/main/java/com/sabi/agent/service/repositories/StateRepository.java
@@ -2,8 +2,13 @@ package com.sabi.agent.service.repositories;
 
 
 
+
 import com.sabi.agent.core.models.State;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -15,4 +20,9 @@ import org.springframework.stereotype.Repository;
 public interface StateRepository extends JpaRepository<State, Long> {
 
     State findByName(String name);
+
+
+    @Query("SELECT s FROM State s WHERE ((:name IS NULL) OR (:name IS NOT NULL AND s.name = :name))")
+    Page<State> findStates(@Param("name")String name, Pageable pageable);
+
 }
