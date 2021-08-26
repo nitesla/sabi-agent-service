c002c02d8eb8b9609f58d804eb18bbf9813538a8
Davyjones <ekenedavidson15@gmail.com>
2021-08-26 18:54:05 +0100

    banks

--- src/main/java/com/sabi/agent/service/repositories/agentRepo/AgentCategoryRepository.java
+++ src/main/java/com/sabi/agent/service/repositories/agentRepo/AgentCategoryRepository.java
@@ -1,7 +1,12 @@
 package com.sabi.agent.service.repositories.agentRepo;
 
 import com.sabi.agent.core.models.agentModel.AgentCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 
 /**
@@ -9,5 +14,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * This interface is responsible for Agent Category crud operations
  */
 
+@Repository
 public interface AgentCategoryRepository extends JpaRepository<AgentCategory, Long> {
+
+    AgentCategory findByName(String name);
+
+
+    @Query("SELECT a FROM AgentCategory a WHERE ((:name IS NULL) OR (:name IS NOT NULL AND a.name = :name))")
+    Page<AgentCategory> findAgentCategories(@Param("name")String name, Pageable pageable);
 }
