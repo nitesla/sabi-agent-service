ab0eda8df1882101e78ed20d67a6b2b21a35f787
Davyjones <ekenedavidson15@gmail.com>
2021-08-30 21:20:55 +0100

    agentUser update

--- src/main/java/com/sabi/agent/service/repositories/agentRepo/AgentRepository.java
+++ src/main/java/com/sabi/agent/service/repositories/agentRepo/AgentRepository.java
@@ -1,13 +1,21 @@
 package com.sabi.agent.service.repositories.agentRepo;
 
+
+
 import com.sabi.agent.core.models.agentModel.Agent;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * This interface is responsible for Agent crud operations
  */
 
+@Repository
 public interface AgentRepository extends JpaRepository<Agent, Long> {
+
+    Agent findByUserId (Long userId);
+
+    Agent findByRegistrationToken (String registrationToken);
+
 }
