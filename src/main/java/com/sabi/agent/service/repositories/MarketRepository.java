34c5010bfacb039dc417e839dd9ea74ab7b44864
AtumaKen <atumaken@gmail.com>
2021-09-02 11:18:28 +0100

    added create market feature

--- src/main/java/com/sabi/agent/service/repositories/MarketRepository.java
+++ src/main/java/com/sabi/agent/service/repositories/MarketRepository.java
@@ -5,6 +5,7 @@ package com.sabi.agent.service.repositories;
 
 
 
+import com.sabi.agent.core.models.Bank;
 import com.sabi.agent.core.models.Market;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -16,4 +17,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MarketRepository extends JpaRepository<Market, Long> {
+    Market findByName (String name);
 }
--- src/main/java/com/sabi/agent/service/services/MarketService.java
+++ src/main/java/com/sabi/agent/service/services/MarketService.java
@@ -1,7 +1,54 @@
 package com.sabi.agent.service.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.sabi.agent.core.dto.requestDto.MarketDto;
+import com.sabi.agent.core.dto.responseDto.MarketResponseDto;
+import com.sabi.agent.core.models.Market;
+import com.sabi.agent.service.helper.Validations;
+import com.sabi.agent.service.repositories.MarketRepository;
+import com.sabi.agent.service.repositories.MarketRepository;
+import com.sabi.framework.exceptions.ConflictException;
+import com.sabi.framework.utils.CustomResponseCode;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class MarketService {
+
+    private MarketRepository marketRepository;
+    private final ModelMapper mapper;
+    private final ObjectMapper objectMapper;
+    private final Validations validations;
+
+
+
+    public MarketService(MarketRepository marketRepository, ModelMapper mapper, ObjectMapper objectMapper, Validations validations) {
+        this.marketRepository = marketRepository;
+        this.mapper = mapper;
+        this.objectMapper = objectMapper;
+        this.validations = validations;
+    }
+
+    /** <summary>
+     * Market creation
+     * </summary>
+     * <remarks>this method is responsible for creation of new Market</remarks>
+     */
+
+    public MarketResponseDto createMarket(MarketDto request) {
+        validations.validateMarket(request);
+        Market market = mapper.map(request, Market.class);
+        Market marketExist = marketRepository.findByName(request.getName());
+        if(marketExist !=null){
+            throw new ConflictException(CustomResponseCode.CONFLICT_EXCEPTION, " Market already exist");
+        }
+        market.setCreatedBy(0l);
+        market.setIsActive(true);
+        market = marketRepository.save(market);
+        log.debug("Create new Market - {}"+ new Gson().toJson(market));
+        return mapper.map(market, MarketResponseDto.class);
+    }
 }
