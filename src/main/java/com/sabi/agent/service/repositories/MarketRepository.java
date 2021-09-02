b2bd2dd9c64c5270de99750e588dec0fcf4adcbb
AtumaKen <atumaken@gmail.com>
2021-09-02 11:51:13 +0100

    added get all market feature

--- src/main/java/com/sabi/agent/service/repositories/MarketRepository.java
+++ src/main/java/com/sabi/agent/service/repositories/MarketRepository.java
@@ -7,7 +7,10 @@ package com.sabi.agent.service.repositories;
 
 import com.sabi.agent.core.models.Bank;
 import com.sabi.agent.core.models.Market;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -18,4 +21,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MarketRepository extends JpaRepository<Market, Long> {
     Market findByName (String name);
+    Page<Market> findMarkets(Pageable pageable);
 }
