7f8bd688185a6349901a29e303ec3b011b34de83
AtumaKen <atumaken@gmail.com>
2021-09-05 14:50:30 +0100

    Finished up Credit level service feature

--- src/main/java/com/sabi/agent/service/helper/Validations.java
+++ src/main/java/com/sabi/agent/service/helper/Validations.java
@@ -6,14 +6,18 @@ import com.sabi.agent.core.dto.requestDto.*;
 import com.sabi.agent.core.models.LGA;
 import com.sabi.agent.core.models.State;
 import com.sabi.agent.core.models.Ward;
+import com.sabi.agent.core.models.agentModel.AgentCategory;
 import com.sabi.agent.service.repositories.LGARepository;
 import com.sabi.agent.service.repositories.StateRepository;
 import com.sabi.agent.service.repositories.WardRepository;
+import com.sabi.agent.service.repositories.agentRepo.AgentCategoryRepository;
 import com.sabi.framework.exceptions.BadRequestException;
 import com.sabi.framework.exceptions.NotFoundException;
 import com.sabi.framework.utils.CustomResponseCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @SuppressWarnings("All")
 @Slf4j
@@ -23,13 +27,16 @@ public class Validations {
     private StateRepository stateRepository;
     private LGARepository lgaRepository;
     private WardRepository wardRepository;
+    private AgentCategoryRepository agentCategoryRepository;
 
 
 
-    public Validations(LGARepository lgaRepository,StateRepository stateRepository,WardRepository wardRepository) {
+    public Validations(LGARepository lgaRepository,StateRepository stateRepository,
+                       WardRepository wardRepository, AgentCategoryRepository agentCategoryRepository) {
         this.lgaRepository = lgaRepository;
         this.stateRepository = stateRepository;
         this.wardRepository = wardRepository;
+        this.agentCategoryRepository = agentCategoryRepository;
     }
 
 
@@ -103,5 +110,15 @@ public class Validations {
         LGA lga = lgaRepository.findById(wardDto.getLgaId())
                 .orElseThrow(() -> new NotFoundException(CustomResponseCode.NOT_FOUND_EXCEPTION,
                         " Enter a valid LGA id!"));
+    }
+
+    public void validatecreditLevel(CreditLevelDto request) {
+        if (request.getLimit() == null || request.getLimit().compareTo(BigDecimal.ZERO) < 0)
+            throw new BadRequestException(CustomResponseCode.BAD_REQUEST,
+                    "Limit cannot be empty or less than zero");
+
+        AgentCategory agentCategory = agentCategoryRepository.findById(request.getAgentCategoryId())
+                .orElseThrow(() -> new NotFoundException(CustomResponseCode.NOT_FOUND_EXCEPTION,
+                        " Enter a valid Agent category id!"));
     }
 }
