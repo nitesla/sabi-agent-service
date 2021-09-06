cd9cfabe47795d36f2593a935bf809a442d41fb2
Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
2021-09-06 13:56:15 +0100

    Fixes

--- src/main/java/com/sabi/agent/service/helper/Validations.java
+++ src/main/java/com/sabi/agent/service/helper/Validations.java
@@ -2,17 +2,18 @@ package com.sabi.agent.service.helper;
 
 
 import com.sabi.agent.core.dto.agentDto.requestDto.AgentCategoryDto;
+import com.sabi.agent.core.dto.agentDto.requestDto.AgentCategoryTargetDto;
 import com.sabi.agent.core.dto.requestDto.*;
 import com.sabi.agent.core.models.LGA;
 import com.sabi.agent.core.models.State;
+import com.sabi.agent.core.models.TargetType;
 import com.sabi.agent.core.models.Ward;
 import com.sabi.agent.core.models.agentModel.AgentCategory;
 import com.sabi.agent.service.repositories.LGARepository;
 import com.sabi.agent.service.repositories.StateRepository;
-import com.sabi.agent.service.repositories.SupervisorRepository;
+import com.sabi.agent.service.repositories.TargetTypeRepository;
 import com.sabi.agent.service.repositories.WardRepository;
 import com.sabi.agent.service.repositories.agentRepo.AgentCategoryRepository;
-import com.sabi.agent.service.repositories.agentRepo.AgentRepository;
 import com.sabi.framework.exceptions.BadRequestException;
 import com.sabi.framework.exceptions.NotFoundException;
 import com.sabi.framework.models.User;
@@ -21,8 +22,6 @@ import com.sabi.framework.utils.CustomResponseCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
-
 @SuppressWarnings("All")
 @Slf4j
 @Service
@@ -30,27 +29,20 @@ public class Validations {
 
     private StateRepository stateRepository;
     private LGARepository lgaRepository;
+    private AgentCategoryRepository agentCategoryRepository;
+    private TargetTypeRepository targetTypeRepository;
     private UserRepository userRepository;
     private WardRepository wardRepository;
-    private AgentRepository agentRepository;
-    private AgentCategoryRepository agentCategoryRepository;
-    private SupervisorRepository supervisorRepository;
 
 
-
-    public Validations(LGARepository lgaRepository,StateRepository stateRepository,
-                       WardRepository wardRepository, AgentRepository agentRepository,
-                       SupervisorRepository supervisorRepository, AgentCategoryRepository agentCategoryRepository) {
-        this.lgaRepository = lgaRepository;
+    public Validations(StateRepository stateRepository, LGARepository lgaRepository, AgentCategoryRepository agentCategoryRepository, TargetTypeRepository targetTypeRepository, UserRepository userRepository, WardRepository wardRepository) {
         this.stateRepository = stateRepository;
-        this.wardRepository = wardRepository;
-        this.agentRepository = agentRepository;
-        this.supervisorRepository = supervisorRepository;
+        this.lgaRepository = lgaRepository;
         this.agentCategoryRepository = agentCategoryRepository;
+        this.targetTypeRepository = targetTypeRepository;
+        this.userRepository = userRepository;
+        this.wardRepository = wardRepository;
     }
-
-
-
 
     public void validateState(StateDto stateDto) {
         if (stateDto.getName() == null || stateDto.getName().isEmpty())
@@ -133,29 +125,17 @@ public class Validations {
             throw new BadRequestException(CustomResponseCode.BAD_REQUEST, "Name cannot be empty");
 
     }
-    public void validateAgentSupervisor(com.sabi.agent.core.dto.agentDto.requestDto.AgentSupervisor request) {
-        if (request.getAgent() == null )
-            throw new BadRequestException(CustomResponseCode.BAD_REQUEST, "Agent cannot be empty");
-        if (request.getSupervisor() == null)
-            throw new BadRequestException(CustomResponseCode.BAD_REQUEST, "Supervisor cannot be empty");
 
-        agentRepository.findById(request.getAgent().getId())
+    public void validateAgentCategoryTarget (AgentCategoryTargetDto agentCategoryTargetDto){
+        if (agentCategoryTargetDto.getName() == null || agentCategoryTargetDto.getName().isEmpty())
+            throw new BadRequestException(CustomResponseCode.BAD_REQUEST, "Name cannot be empty");
+
+        AgentCategory agentCategory =  agentCategoryRepository.findById(agentCategoryTargetDto.getAgentCategoryId())
                 .orElseThrow(() -> new NotFoundException(CustomResponseCode.NOT_FOUND_EXCEPTION,
-                        " Enter a valid Agent"));
-        supervisorRepository.findById(request.getSupervisor().getId())
+                        " Enter a valid Agent Category!"));
+
+        TargetType targetType = targetTypeRepository.findById(agentCategoryTargetDto.getTargetTypeId())
                 .orElseThrow(() -> new NotFoundException(CustomResponseCode.NOT_FOUND_EXCEPTION,
-                        " Enter a valid supervisor"));
+                        " Enter a valid Target Type!"));
     }
-
-    public void validatecreditLevel(CreditLevelDto request) {
-        if (request.getLimit() == null || request.getLimit().compareTo(BigDecimal.ZERO) < 0)
-            throw new BadRequestException(CustomResponseCode.BAD_REQUEST,
-                    "Limit cannot be empty or less than zero");
-
-        AgentCategory agentCategory = agentCategoryRepository.findById(request.getAgentCategoryId())
-                .orElseThrow(() -> new NotFoundException(CustomResponseCode.NOT_FOUND_EXCEPTION,
-                        " Enter a valid Agent category id!"));
-    }
-
-
 }
