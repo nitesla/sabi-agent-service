cbee4633bcf83b4dee367348990e9c415fa58336
AtumaKen <atumaken@gmail.com>
2021-09-14 08:52:25 +0100

    update

--- src/main/java/com/sabi/agent/service/repositories/agentRepo/AgentSupervisorRepository.java
+++ src/main/java/com/sabi/agent/service/repositories/agentRepo/AgentSupervisorRepository.java
@@ -2,11 +2,7 @@ package com.sabi.agent.service.repositories.agentRepo;
 
 import com.sabi.agent.core.dto.agentDto.requestDto.AgentSupervisorDto;
 import com.sabi.agent.core.models.agentModel.AgentSupervisor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
