889f39aee87f68c7db8d3ae3e31512cc76c596f9
muyiwa-spinel <muyiwa.a@spinel.consulting>
2021-11-26 06:22:18 -0800

    wishList update

---
+++ src/main/java/com/sabi/agent/service/services/WishListService.java
@@ -0,0 +1,12 @@
+package com.sabi.agent.service.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@SuppressWarnings("ALL")
+@Slf4j
+@Service
+public class WishListService {
+
+
+}
