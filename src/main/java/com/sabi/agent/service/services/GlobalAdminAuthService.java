0d0d323bb5ec31e858bf6eec931b28d4d3527edf
Afam Okonkwo <afam.o@spinel.consulting>
2022-05-20 10:00:45 +0100

    global admin completion

--- src/main/java/com/sabi/agent/service/services/GlobalAdminAuthService.java
+++ src/main/java/com/sabi/agent/service/services/GlobalAdminAuthService.java
@@ -4,10 +4,11 @@ import com.sabi.agent.core.dto.requestDto.GlobalAdminAuthRequestDto;
 import com.sabi.agent.core.dto.responseDto.AuthenticateUserResponseDto;
 import com.sabi.agent.core.dto.responseDto.GlobalAdminAuthResponse;
 import com.sabi.framework.exceptions.BadRequestException;
+import com.sabi.framework.exceptions.ConflictException;
+import com.sabi.framework.exceptions.LockedException;
 import com.sabi.framework.helpers.API;
 import com.sabi.framework.models.User;
 import com.sabi.framework.notification.requestDto.NotificationRequestDto;
-import com.sabi.framework.notification.requestDto.RecipientRequest;
 import com.sabi.framework.repositories.UserRepository;
 import com.sabi.framework.security.AuthenticationWithToken;
 import com.sabi.framework.service.NotificationService;
@@ -17,12 +18,15 @@ import com.sabi.framework.service.UserService;
 import com.sabi.framework.utils.Constants;
 import com.sabi.framework.utils.CustomResponseCode;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.net.ConnectException;
+import java.net.UnknownHostException;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -42,104 +46,168 @@ public class GlobalAdminAuthService {
 
     private final PasswordEncoder passwordEncoder;
 
+    private final ModelMapper modelMapper;
+
     @Value("${global_url}")
     private String globalUrl;
 
     @Value("${loginUrl}")
     private String loginUrl;
 
-    @Value("{agent.code}")
+    @Value("${agent.code}")
     private String agentCode;
+
+    @Value("${login.attempts}")
+    private int loginAttempts;
+    private int loginCounter;
 
     private final NotificationService notificationService;
 
-    public GlobalAdminAuthService(API api, UserRepository userRepository, TokenService tokenService, UserService userService, PermissionService permissionService, PasswordEncoder passwordEncoder, NotificationService notificationService) {
+    public GlobalAdminAuthService(API api, UserRepository userRepository, TokenService tokenService, UserService userService, PermissionService permissionService, PasswordEncoder passwordEncoder, ModelMapper modelMapper, NotificationService notificationService) {
         this.api = api;
         this.userRepository = userRepository;
         this.tokenService = tokenService;
         this.userService = userService;
         this.permissionService = permissionService;
         this.passwordEncoder = passwordEncoder;
+        this.modelMapper = modelMapper;
         this.notificationService = notificationService;
     }
 
+    /**
+     * @Description: I modified this method to return many other needed parameters after authenticating with globalAdmin
+     * @Date: 19/05/2022
+     * @Author: Afam Okonkwo
+     * @param requestDto
+     * @return
+     */
     public GlobalAdminAuthResponse authenticateUser(GlobalAdminAuthRequestDto requestDto) {
         requestDto.setApplicationCode(agentCode);
         String accessToken = null;
         GlobalAdminAuthResponse globalAdminAuthResponse = new  GlobalAdminAuthResponse();
         NotificationRequestDto notificationRequestDto = new NotificationRequestDto();
-        AuthenticateUserResponseDto authResponse = authenticateUser(
-                requestDto.getApplicationCode(),requestDto.getAuthKey(),
-                requestDto.getUserId());
-        log.info("Response for Global Admin ::::::::::::::::::::::::: {} ", authResponse);
-        if (!authResponse .getCode().equals("200") ){
-            log.info("404 Error logged ::::::::::::::::::::::");
-            throw new BadRequestException(CustomResponseCode.UNAUTHORIZED, "Unauthorized");
-        }else{
-            User user = confirmUser(authResponse);
-            if (user != null){
-                log.info("User should be logged in :::::::::::::::::: ");
-                accessToken = "Bearer" +" "+this.tokenService.generateNewToken();
-            } else {
-                Random rand = new Random();
-                User userTosave = new User();
-                String passwrd = String.valueOf(rand.nextInt(9999999));
-                userTosave.setFirstName(authResponse.getData().getFirstName());
-                userTosave.setLastName(authResponse.getData().getLastName());
-                userTosave.setPassword(passwordEncoder.encode(passwrd));
-                userTosave.setPhone(authResponse.getData().getPhone());
-                userTosave.setEmail(authResponse.getData().getUsername());
-                userTosave.setUsername(authResponse.getData().getUsername());
-                userTosave.setLoginAttempts(0);
-                userTosave.setUserCategory(Constants.GLOBAL_ADMIN);
-                userTosave.setIsActive(true);
-                userTosave.setPasswordChangedOn(LocalDateTime.now());
-                userTosave.setCreatedBy(0L);
-                userTosave.setCreatedDate(LocalDateTime.now());
-                userTosave.setUpdatedDate(LocalDateTime.now());
-                userRepository.save(userTosave);
-                String accessList = permissionService.getPermissionsByUserId(user.getId());
-                AuthenticationWithToken authWithToken = new AuthenticationWithToken(user, null,
-                        AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER,"+accessList));
-                accessToken = "Bearer" +" "+this.tokenService.generateNewToken();
-                authWithToken.setToken(accessToken);
-                tokenService.store(accessToken, authWithToken);
-                SecurityContextHolder.getContext().setAuthentication(authWithToken);
-                userService.updateLogin(user.getId());
-//                         authenticateLogin(user.getUsername(),passwrd);
-                notificationRequestDto.setMessage("User Default password is : " + " " + passwrd);
-                log.info("New User Password ::::::::::::::: " + passwrd);
-                List<RecipientRequest> recipient = new ArrayList<>();
-                recipient.add(RecipientRequest.builder()
-                        .email(user.getEmail())
-                        .build());
-                notificationRequestDto.setRecipient(recipient);
-                notificationService.emailNotificationRequest(notificationRequestDto);
-            }
-            String accessList = permissionService.getPermissionsByUserId(user.getId());
-            AuthenticationWithToken authWithToken = new AuthenticationWithToken(user, null,
-                    AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER,"+accessList));
-            accessToken = "Bearer" +" "+this.tokenService.generateNewToken();
-            authWithToken.setToken(accessToken);
-            tokenService.store(accessToken, authWithToken);
-            SecurityContextHolder.getContext().setAuthentication(authWithToken);
-            userService.updateLogin(user.getId());
-            globalAdminAuthResponse.setToken(accessToken);
-            log.info("User Access token ::::::::::::::::::::: " + globalAdminAuthResponse.getToken());
+        AuthenticateUserResponseDto authResponse = null;
+        try {
+            authResponse = authenticateUser(
+                    requestDto.getApplicationCode(),requestDto.getAuthKey(),
+                    requestDto.getUserId());
+            log.info("Response for Global Admin ::::::::::::::::::::::::: {} ", authResponse);
+            if (authResponse!= null && !authResponse .getCode().equals("200") ){
+                log.info("404 Error logged ::::::::::::::::::::::");
+                throw new BadRequestException(CustomResponseCode.UNAUTHORIZED, "Unauthorized");
+            }else{
+                User user = confirmUser(authResponse);
+                if (user != null){
+                    log.info("User should be logged in :::::::::::::::::: ");
+                    /**
+                     * @Author: Afam Okonkwo
+                     * @Date: 19/05/22
+                     * @Description: Returns some other important information other than just token
+                     */
+                    globalAdminAuthResponse = modelMapper.map(user,GlobalAdminAuthResponse.class);
+                    globalAdminAuthResponse.setUserId(user.getId());
+                    accessToken = setUpStoreAndGetAccessToken(user);
+                } else {
+                    Random rand = new Random();
+                    User userTosave = new User();
+                    /**
+                     * @Author: Afam Okonkwo
+                     * @Date: 19/05/22
+                     *@Description: Mr Coker said we should initiate password generation for the admin
+                     */
+                    //String passwrd = String.valueOf(rand.nextInt(9999999));
+                    userTosave.setFirstName(authResponse.getData().getFirstName());
+                    userTosave.setLastName(authResponse.getData().getLastName());
 
+                    //userTosave.setPassword(passwordEncoder.encode(passwrd));
+                    userTosave.setPhone(authResponse.getData().getPhone());
+                    userTosave.setEmail(authResponse.getData().getUsername());
+                    userTosave.setUsername(authResponse.getData().getUsername());
+                    userTosave.setLoginAttempts(0);
+                    userTosave.setUserCategory(Constants.GLOBAL_ADMIN);
+                    userTosave.setIsActive(true);
+                    userTosave.setPasswordChangedOn(LocalDateTime.now());
+                    userTosave.setCreatedBy(0L);
+                    userTosave.setCreatedDate(LocalDateTime.now());
+                    userTosave.setUpdatedDate(LocalDateTime.now());
+                    userTosave.setGlobalAdminUserId(authResponse.getData().getUserId());
+                    userRepository.save(userTosave);
+                    globalAdminAuthResponse = modelMapper.map(userTosave, GlobalAdminAuthResponse.class);
+                    globalAdminAuthResponse.setUserId(userTosave.getId());
+                    accessToken = setUpStoreAndGetAccessToken(userTosave);
+//                         authenticateLogin(user.getUsername(),passwrd);
+                    /**
+                     * @Author: Afam Okonkwo
+                     * @Date: 19/05/22
+                     * @Description: Mr Coker said we should not initiate password generation nor sending email for the admin
+                     */
+                    /**
+                     notificationRequestDto.setMessage("User Default password is : " + " " + passwrd);
+                     log.info("New User Password ::::::::::::::: " + passwrd);
+                     List<RecipientRequest> recipient = new ArrayList<>();
+                     recipient.add(RecipientRequest.builder()
+                     .email(user.getEmail())
+                     .build());
+                     notificationRequestDto.setRecipient(recipient);
+                     notificationService.emailNotificationRequest(notificationRequestDto);
+                     */
+                }
+                /**
+                 * @Author: Afam Okonkwo
+                 * @Date: 19/05/22
+                 * @Description: Returns some other important information other than just token
+                 */
+                globalAdminAuthResponse.setAccessToken(accessToken);
+                globalAdminAuthResponse.setLastLogin(LocalDateTime.now());
+                globalAdminAuthResponse.setTokenExpiry(this.userService.getSessionExpiry());
+                globalAdminAuthResponse.setGlobalAdminUserId(authResponse.getData().getUserId());
+                globalAdminAuthResponse.setApplicationCode(this.agentCode);
+                globalAdminAuthResponse.setGlobalAdminToken(authResponse.getData().getToken());
+                globalAdminAuthResponse.setActionDate(authResponse.getData().getActionDate());
+                globalAdminAuthResponse.setAuthKeyExpirationDate(authResponse.getData().getAuthKeyExpirationDate());
+
+                log.info("User Access token ::::::::::::::::::::: " + globalAdminAuthResponse.getAccessToken());
+            }
+            return globalAdminAuthResponse;
+        } catch (UnknownHostException unknownHostException) {
+            throw new ConflictException(CustomResponseCode.CONFLICT_EXCEPTION,"Make sure you have internet connection");
         }
-        return globalAdminAuthResponse;
+
+    }
+
+    private String setUpStoreAndGetAccessToken(User user) {
+        String accessToken;
+        String accessList = permissionService.getPermissionsByUserId(user.getId());
+        log.info("The AccessList is  :::{}",accessList);
+        AuthenticationWithToken authWithToken = new AuthenticationWithToken(user, null,
+                AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER,"+accessList));
+        accessToken = "Bearer" +" "+this.tokenService.generateNewToken();
+        authWithToken.setToken(accessToken);
+        tokenService.store(accessToken, authWithToken);
+        SecurityContextHolder.getContext().setAuthentication(authWithToken);
+        userService.updateLogin(user.getId());
+        return accessToken;
+    }
+
+    private void chekLoginAttemptsAndLockAccount(User user) {
+        if (this.loginAttempts > loginCounter) {
+            loginCounter++;
+        } else {
+            throw new LockedException("423", "Your account is locked. It must be unlocked before you can login");
+        }
     }
 
 
     public AuthenticateUserResponseDto authenticateUser(
-            String applicationCode, String authKey, String userId) {
+            String applicationCode, String authKey, String userId) throws UnknownHostException {
         AuthenticateUserResponseDto response = api.get(
                 globalUrl + "appinfo/authkey"+"?"+"applicationCode="+applicationCode+"&"+"authKey="+
                         authKey+"&"+"userId="+userId,
                 AuthenticateUserResponseDto.class,getHeaders());
         log.info("Response of associated account ::::::::::::::: " + response);
         return response;
+
+
     }
 
     private User confirmUser(AuthenticateUserResponseDto responseDto){
